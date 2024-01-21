16c04d9ad0ad13fbaf0238cbe061840c2ceb5baa
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-01-21 18:19:40 +0100

    UPDATE top title size

--- src/components/TopTitle.vue
+++ src/components/TopTitle.vue
@@ -5,5 +5,5 @@ export default {
 </script>
 
 <template>
-    <h1 class="text-8xl text-gray-200 px-5 py-10 col-span-full h-fit">Thomas MARQUES</h1>
+    <h1 class="text-6xl text-gray-200 px-5 py-10 col-span-full h-fit">Thomas MARQUES</h1>
 </template>
