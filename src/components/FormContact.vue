364321809d7dd612eeab1749d0b2395723a84cf2
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-01-30 17:09:43 +0100

    REMOVE unused css files

---
+++ src/components/FormContact.vue
@@ -0,0 +1,40 @@
+<script>
+import emailjs from '@emailjs/browser';
+
+export default {
+    name: 'FormContact',
+    methods: {
+        sendEmail() {
+            emailjs.sendForm('service_hqep5yi', 'template_6okmd4l', this.$refs.form, 'jsS5BIOuCJHSI1yLF')
+            .then((result) => {
+                console.log('SUCCESS!', result.text);
+            }, (error) => {
+                console.log('FAILED...', error.text);
+            });
+        }
+    }
+
+}
+</script>
+
+<template>
+    <form ref="form" @submit.prevent="sendEmail" class="flex flex-col w-full text-gray-200 p-4">
+            <div class="flex gap-8 max-[800px]:flex-col">
+                <div class="flex flex-col w-full">
+                    <div class="flex flex-col relative">
+                        <label class="mt-4 text-lg">Name</label>
+                        <input type="text" name="from_name" class="bg-zinc-800 rounded-md p-2 focus:bg-gray-700 focus:outline-none" required="required">
+                    </div>
+                    <div class="flex flex-col">
+                        <label class="mt-4 text-lg">Email</label>
+                        <input type="email" name="from_email" class="bg-zinc-800 rounded-md p-2 focus:bg-gray-700 focus:outline-none" required="required">
+                    </div>
+                </div>
+                <div class=" flex flex-col w-full">
+                    <label class="mt-4 text-lg">Message</label>
+                    <textarea name="message" class="bg-zinc-800 rounded-md p-2 h-full min-h-60 resize-none focus:bg-gray-700 focus:outline-none" required="required"></textarea>
+                </div>
+            </div>
+            <button type="submit" class="flex gap-2 items-center cursor-pointer bg-zinc-600 w-fit px-4 py-2 rounded-lg self-center mt-6 text-lg hover:bg-gray-700 hover:text-gray-400">Send <i class="fa-solid fa-paper-plane"></i></button>
+        </form>
+</template>
---
+++ src/components/SkillsSection.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'SkillsSection',
+}
+</script>
+
+<template>
+    <div class="grid grid-cols-3 justify-items-center text-gray-400 h-fit gap-4 mx-5 mt-6 max-[650px]:grid-cols-1">
+          <div class="flex flex-col items-center gap-5 w-full relative border-2 border-gray-400 p-5">
+            <h3 class="bg-zinc-900 absolute -top-5 p-2 font-bold">Languages</h3>
+            <span class="flex flex-wrap justify-center items-center gap-3 w-full mt-6">
+              <i class="devicon-html5-plain text-4xl hover:text-red-500 hover:[text-shadow:_0_0_20px_rgb(239_68_68_/_40%)]"></i>
+              <i class="devicon-css3-plain text-4xl hover:text-blue-500 hover:[text-shadow:_0_0_20px_rgb(59_130_246_/_40%)]"></i>
+              <i class="devicon-javascript-plain text-4xl hover:text-yellow-500 hover:[text-shadow:_0_0_20px_rgb(234_179_8_/_40%)]"></i>
+              <i class="devicon-php-plain text-4xl hover:text-cyan-700 hover:[text-shadow:_0_0_20px_rgb(14_116_144_/_40%)]"></i>
+              <i class="devicon-mysql-plain text-4xl hover:text-gray-50 hover:[text-shadow:_0_0_20px_rgb(249_250_251_/_40%)]"></i>
+              <i class="devicon-python-plain text-4xl hover:text-yellow-400 hover:[text-shadow:_0_0_20px_rgb(250_204_21_/_40%)]"></i>
+            </span>
+          </div>
+          <div class="flex flex-col items-center gap-5 w-full relative border-2 border-gray-400 p-5">
+            <h3 class="bg-zinc-900 absolute -top-5 p-2 font-bold">Frameworks</h3>
+            <span class="flex flex-wrap justify-center items-center gap-3 w-full mt-6">
+              <i class="devicon-vuejs-plain text-4xl hover:text-green-400 hover:[text-shadow:_0_0_20px_rgb(74_222_128_/_40%)]"></i>
+              <i class="devicon-react-original text-4xl hover:text-cyan-400 hover:[text-shadow:_0_0_20px_rgb(34_211_238_/_40%)]"></i>
+              <i class="devicon-tailwindcss-plain text-4xl hover:text-cyan-900 hover:[text-shadow:_0_0_20px_rgb(22_78_99_/_40%)]"></i>
+            </span>
+          </div>
+          <div class="flex flex-col items-center gap-5 w-full relative border-2 border-gray-400 p-5">
+            <h3 class="bg-zinc-900 absolute -top-5 p-2 font-bold">Tools</h3>
+            <span class="flex flex-wrap justify-center items-center gap-3 w-full mt-6">
+              <i class="devicon-figma-plain text-4xl hover:text-red-400 hover:[text-shadow:_0_0_20px_rgb(248_113_113_/_40%)]"></i>
+              <i class="devicon-wordpress-plain text-4xl hover:text-teal-950 hover:[text-shadow:_0_0_20px_rgb(4_47_46_/_40%)]"></i>
+            </span>
+          </div>
+        </div>
+</template>
